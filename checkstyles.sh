9b415a98079885def42a493dcdc1144efe04131b
Ben <bentaylortheonly@gmail.com>
2014-10-05 20:48:34 +0100

    routing works

--- checkstyles.sh
+++ checkstyles.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-java -jar mkgmap/dist/mkgmap.jar --style-file=styles --style=default --check-styles
-java -jar mkgmap/dist/mkgmap.jar --style-file=styles --style=clean --check-styles
+for D in styles/*; do
+	if [ -d "${D}" ]; then
+		style=`basename $D`
+		java -jar mkgmap/dist/mkgmap.jar --style-file=styles --style=$style --check-styles
+		echo ""
+	fi
+done
