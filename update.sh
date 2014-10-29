a1651ab4ce91de57f13b053b5692153c23313df7
Ben <bentaylortheonly@gmail.com>
2014-10-29 19:26:37 +0000

    update does daily only and logs

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "  running osmupdate"
-./osmupdate $1.osm.pbf $1.updated.osm
+./osmupdate $1.osm.pbf $1.updated.osm --day --verbose 1
 echo "  converting $1 to binary format"
 ./osmconvert $1.updated.osm --out-pbf>$1.osm.pbf
 rm $1.updated.osm
